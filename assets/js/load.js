aaf5fc554c8f79af5bac265c4ef14266f9bc2ea1
marygale <mary.jabagat@corp.mobile.co>
2016-03-26 17:28:19 +0800

    - changes on load.js

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,7 +1,6 @@
 head.load(
     { file: 'assets/js/jquery.min.js' },
-    //{ file: 'assets/js/libs/angular/angular.min.js' },
-    { file: '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-rc.0/angular.js' },
+    { file: 'assets/js/libs/angular/angular.min.js' },
     { file: 'assets/js/libs/angular/angular-route.min.js' },
     { file: 'assets/js/libs/angular/angular-animate.min.js' },
     { file: 'assets/js/libs/angular/ngDialog.min.js' },
