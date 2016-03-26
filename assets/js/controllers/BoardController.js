13dcc8daf172b411b2bcdb221d86b4a60e652150
marygale <mary.jabagat@corp.mobile.co>
2016-03-26 17:25:01 +0800

    -refromat code

--- assets/js/controllers/BoardController.js
+++ assets/js/controllers/BoardController.js
@@ -1,76 +1,52 @@
 angular.module('appController')
-    .controller("boardCtrl", function($scope, Status, colours, ngDialog){
+    .controller("boardCtrl", function ($scope, Status, colours, ngDialog) {
         var vm = this;
 
-        vm.archive ={
-            show : false,
+        vm.archive = {
+            show: false,
             class: '',
         }
 
         $scope.count = 0;
         $scope.statuses = Status;
-        $scope.show=false;
+        $scope.show = false;
         $scope.colours = colours;
 
-        $scope.extractData = function(){
-            return JSON.parse(localStorage.getItem("dashboards")) || [
-                    {id: '1', title: 'Learn AngularJS', status: 'Backlog', trash: false, color: colours[0]},
-                    {id: '2', title: 'Build an Angular app Build an Angular app Build an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular appBuild an Angular app', status: 'In Progress', trash: false, color:colours[1]},
-                    {id: '3', title: 'Mobile Application', status: 'In Progress', trash: false, color:colours[2]},
-                    {id: '4', title: 'Mobile Application1', status: 'Done', trash: false, color:colours[2]},
-                    {id: '5', title: 'Mobile Application2', status: 'In Progress', trash: false, color:colours[2]},
-                    {id: '6', title: 'Mobile Application3', status: 'In Progress', trash: false, color:colours[2]},
-                    {id: '7', title: 'Mobile Application4', status: 'In Progress', trash: false, color:colours[2]},
-                    {id: '8', title: 'Mobile Application5', status: 'In Progress', trash: false, color:colours[2]},
-                    {id: '9', title: 'Mobile Application6', status: 'In Progress', trash: false, color:colours[2]},
-                    {id: '10', title: 'Mobile Application7', status: 'Done', trash: false, color:colours[2]},
-                    {id: '11', title: 'Mobile Application8', status: 'Done', trash: false, color:colours[2]},
-                    {id: '12', title: 'Mobile Application9', status: 'Done', trash: false, color:colours[2]},
-                    {id: '13', title: 'Mobile Application10', status: 'Done', trash: false, color:colours[2]},
-                    {id: '14', title: 'Mobile Application11', status: 'Done', trash: true, color:colours[2]},
-                    {id: '15', title: 'Mobile Application12', status: 'Done', trash: true, color:colours[2]},
-                    {id: '16', title: 'Mobile Application13', status: 'Done', trash: true, color:colours[2]},
-                    {id: '17', title: 'Mobile Application14', status: 'Done', trash: true, color:colours[2]},
-                    {id: '18', title: 'Mobile Application15', status: 'Done', trash: true, color:colours[2]},
-                    {id: '19', title: 'Mobile Application16', status: 'Done', trash: true, color:colours[2]},
-                    {id: '20', title: 'Mobile Application17', status: 'Done', trash: true, color:colours[2]},
-                    {id: '21', title: 'Mobile Application18', status: 'Done', trash: true, color:colours[2]}
-
-                ];
+        $scope.extractData = function () {
+            return JSON.parse(localStorage.getItem("dashboards")) || [];
         }
         $scope.dashboards = $scope.extractData();
 
-       //localStorage.clear();
-
-
+        //localStorage.clear();
         console.log($scope.dashboards);
         vm.actions = {
-            'addToBoards':function(){
-                angular.forEach(vm.card,function(section, name) {
-                        vm.card[name] = section;
+            'addToBoards': function () {
+                angular.forEach(vm.card, function (section, name) {
+                    vm.card[name] = section;
                 });
-                $scope.dashboards.push(vm.card); console.log($scope.dashboards);
+                $scope.dashboards.push(vm.card);
+                console.log($scope.dashboards);
                 localStorage.setItem('dashboards', JSON.stringify($scope.dashboards));
                 vm.actions.init();
             },
-            'isEmptyStatus' : function(status){
+            'isEmptyStatus': function (status) {
                 var empty = true;
-                if ($scope.dashboards) {
+                if ($scope.dashboards && status) {
                     $scope.dashboards.forEach(function (story) {
-                        if (story.status === status.name) empty = false;
+                        if (story.status === status) empty = false;
                     });
                 }
 
                 return empty;
             },
             'changeStatus': function (story, status) {
-                    story.status = status.name;
+                story.status = status.name;
             },
             'insertAdjacent': function (target, story, insertBefore) {
                 if (target === story) return;
 
                 var fromIdx = $scope.dashboards.indexOf(story);
-                var toIdx   = $scope.dashboards.indexOf(target);
+                var toIdx = $scope.dashboards.indexOf(target);
 
                 if (!insertBefore) toIdx++;
 
@@ -83,39 +59,40 @@ angular.module('appController')
                     story.status = target.status;
                 }
             },
-            'finalizeDrop': function(story){
-                angular.forEach($scope.dashboards,function(section, idx){
-                    if($scope.dashboards[idx].id == story.id){
+            'finalizeDrop': function (story) {
+                angular.forEach($scope.dashboards, function (section, idx) {
+                    if ($scope.dashboards[idx].id == story.id) {
                         $scope.dashboards[idx].status = story.status
                         console.log('finalizeDrop ', $scope.dashboards);
-                       localStorage.setItem('dashboards', JSON.stringify($scope.dashboards));
+                        localStorage.setItem('dashboards', JSON.stringify($scope.dashboards));
                     }
                 });
 
             },
-            'modalToggle': function(action, story, stat){
-                if(action== 'edit'){
+            'modalToggle': function (action, story, stat) {
+                if (action == 'edit') {
                     vm.card.title = story.title;
                     vm.card.color = story.color;
-                    vm.card.id    = story.id;
-                    $scope.story  = story;
-                }else{
+                    vm.card.id = story.id;
+                    $scope.story = story;
+                } else {
                     vm.actions.init();
                     vm.card.status = stat;
                 }
-                $scope.action= action;
+                $scope.action = action;
                 ngDialog.openConfirm({
                     template: 'src/templates/forms.html',
                     className: 'ngdialog-theme-default',
                     scope: $scope,
                 })
-                    .then(function(){
-                        if(action == 'add'){
+                    .then(function () {
+                        if (action == 'add') {
                             vm.actions.addToBoards();
-                        }else if(action == 'edit'){ console.log('$scope.story ',$scope.story);
-                            angular.forEach($scope.dashboards,function(section, idx){
-                                if($scope.dashboards[idx]['id'] == story.id){
-                                    angular.forEach(vm.card,function(section, name) {
+                        } else if (action == 'edit') {
+                            console.log('$scope.story ', $scope.story);
+                            angular.forEach($scope.dashboards, function (section, idx) {
+                                if ($scope.dashboards[idx]['id'] == story.id) {
+                                    angular.forEach(vm.card, function (section, name) {
                                         $scope.dashboards[idx][name] = section;
                                     });
                                     console.log('edit ', $scope.dashboards[idx]);
@@ -124,31 +101,31 @@ angular.module('appController')
                             });
                         }
 
-                    }, function(value){
+                    }, function (value) {
                         console.log('rejected:' + value);
 
                     });
             },
-            'deleteAllCardInColumn': function(story){
-                angular.forEach($scope.dashboards,function(section, name) {
-                    if($scope.dashboards[name].trash == story.trash) {
+            'deleteAllCardInColumn': function (story) {
+                angular.forEach($scope.dashboards, function (section, name) {
+                    if ($scope.dashboards[name].trash == story.trash) {
                         $scope.dashboards[name].trash = true;
                     }
                 });
                 localStorage.setItem('dashboards', JSON.stringify($scope.dashboards));
             },
-            'toTrash': function(story, entireCol){
+            'toTrash': function (story, entireCol) {
                 ngDialog.openConfirm({
                     template: 'modalDialogId',
                     className: 'ngdialog-theme-default'
                 }).then(function () {
-                    angular.forEach($scope.dashboards,function(section, name) {
-                        if(entireCol){
-                            if($scope.dashboards[name].status == story) {
+                    angular.forEach($scope.dashboards, function (section, name) {
+                        if (entireCol) {
+                            if ($scope.dashboards[name].status == story) {
                                 $scope.dashboards[name].trash = true;
                             }
-                        }else{
-                            if($scope.dashboards[name].id == story.id) {
+                        } else {
+                            if ($scope.dashboards[name].id == story.id) {
                                 $scope.dashboards[name].trash = true;
                             }
                         }
@@ -160,9 +137,9 @@ angular.module('appController')
                     console.log('Modal promise rejected.');
                 });
             },
-            'init': function(){
+            'init': function () {
                 vm.card = {
-                    id:    vm.actions.generateId().new(),
+                    id: vm.actions.generateId().new(),
                     title: 'Story #1',
                     trash: false,
                     color: colours[0]['name']
@@ -170,32 +147,32 @@ angular.module('appController')
                 $scope.statuses = Status;
 
             },
-            'generateId': function(){
+            'generateId': function () {
                 return {
-                    new: function() {
-                            return (Math.random().toString(16)+"000000000").substr(2,8);
+                    new: function () {
+                        return (Math.random().toString(16) + "000000000").substr(2, 8);
                     },
-                    empty: function() {
+                    empty: function () {
                         return '';
                     }
                 };
             },
-            'showArchiveBox': function(show){
-                if(show == false) {
+            'showArchiveBox': function (show) {
+                if (show == false) {
                     vm.archive.show = true;
                     vm.archive.class = 'opened';
                 }
-                if(show == true) {
+                if (show == true) {
                     vm.archive.show = false;
                     vm.archive.class = '';
                 }
             },
-            'superDelete': function(){
+            'superDelete': function () {
                 ngDialog.openConfirm({
                     template: 'modalDialogId',
                     className: 'ngdialog-theme-default'
                 }).then(function () {
-                    for (var i = 0; i < $scope.dashboards.length; i++){
+                    for (var i = 0; i < $scope.dashboards.length; i++) {
                         if ($scope.dashboards[i].trash && $scope.dashboards[i].trash === true) {
                             $scope.dashboards.splice(i);
                             $scope.count = 0;
@@ -208,18 +185,19 @@ angular.module('appController')
                 });
 
             },
-            'checkArchive': function(){
-                angular.forEach($scope.dashboards,function(section) {
-                    if(section.trash == true) {
-                        $scope.count+= 1;
+            'checkArchive': function () {
+                angular.forEach($scope.dashboards, function (section) {
+                    if (section.trash == true) {
+                        $scope.count += 1;
                     }
 
                 });
-                console.log('list ',$scope.count);
+                console.log('list ', $scope.count);
             }
 
         }
         vm.actions.init();
         vm.actions.checkArchive();
+        vm.actions.isEmptyStatus();
 
     });
