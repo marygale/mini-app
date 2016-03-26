bbd76d5a6fce871f5e00c12f5945ffb29411c2a6
marygale <mary.jabagat@corp.mobile.co>
2016-03-26 17:59:46 +0800

    -add tooltip directives

--- assets/js/directives/directives.js
+++ assets/js/directives/directives.js
@@ -85,4 +85,16 @@ app.directive('dropTarget', function ($parse) {
             }
         };
 });
-
+app.directive('toggle', function(){
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs){
+            if (attrs.toggle=="tooltip"){
+                $(element).tooltip();
+            }
+            if (attrs.toggle=="popover"){
+                $(element).popover();
+            }
+        }
+    };
+});
